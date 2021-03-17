ed8570e1445b85dc45c0ffadd38f771e57e9609a
lijunlin <327603258@qq.com>
2021-03-17 10:40:19 +0800

    nullsafety

--- lib/flutterflappyaes.dart
+++ lib/flutterflappyaes.dart
@@ -11,31 +11,31 @@ class Flutterflappyaes {
   static const MethodChannel _channel = const MethodChannel('flutterflappyaes');
 
   //进行aes加密
-  static Future<String> aesEncryptCBC(
+  static Future<String?> aesEncryptCBC(
       String data, String aeskey, String iv) async {
-    final String aes = await _channel
+    final String? aes = await _channel
         .invokeMethod('aesEncryptCBC', {"data": data, "aeskey": aeskey, "iv": iv});
     return aes;
   }
 
   //进行aesecb加密
-  static Future<String> aesEncryptECB(String data, String aeskey) async {
-    final String aes = await _channel
+  static Future<String?> aesEncryptECB(String data, String aeskey) async {
+    final String? aes = await _channel
         .invokeMethod('aesEncryptECB', {"data": data, "aeskey": aeskey});
     return aes;
   }
 
   //进行aes解密
-  static Future<String> aesDecryptCBC(
+  static Future<String?> aesDecryptCBC(
       String data, String aeskey, String iv) async {
-    final String aes = await _channel
+    final String? aes = await _channel
         .invokeMethod('aesDecryptCBC', {"data": data, "aeskey": aeskey, "iv": iv});
     return aes;
   }
 
   //进行aesecb解密
-  static Future<String> aesDecryptECB(String data, String aeskey) async {
-    final String aes = await _channel.invokeMethod('aesDecryptECB', {
+  static Future<String?> aesDecryptECB(String data, String aeskey) async {
+    final String? aes = await _channel.invokeMethod('aesDecryptECB', {
       "data": data,
       "aeskey": aeskey,
     });
